b0fc66cf35f572df2de64878c1c8b8076638a4b7
yanzd <18235125690@163.com>
2021-01-30 12:01:28 +0800

    引入方式变更

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import './index.css'
+import  "tailwindcss/tailwind.css"
 
 createApp(App).mount('#app')
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
 module.exports = {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: 'media', // or 'media' or 'class'
   theme: {
     extend: {},
+    container: {
+      center: true,
+    }
   },
   variants: {
     extend: {},
